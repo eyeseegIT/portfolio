f849465d85c4e3e983fda35b1c2116c67fa918c5
Angela Quach <angelayunquach@gmail.com>
2022-01-23 09:50:10 -0500

    working hover effect for projects

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -16,8 +16,12 @@ const Project = () => {
       <div className="card">
         <div className="card-content">
           <div className="card-img-container">
-            <a target="_blank" rel="noopener noreferrer" href="https://foodie-baby.herokuapp.com/"><img src={ FoodieBaby } alt="foodie baby" className="card-img"></img></a>
-            <h2 className="card-description">Foodie Baby</h2>
+            <a target="_blank" rel="noopener noreferrer" href="https://foodie-baby.herokuapp.com/"><img src={ FoodieBaby } alt="foodie baby" className="card-img"></img>
+            <div className="card-description">
+              <h1>Foodie Baby</h1>
+              <p>Help parents decide. Built using MEN stack.</p>
+            </div>
+            </a>
           </div>
         </div>
       </div>
@@ -25,8 +29,12 @@ const Project = () => {
       <div className="card">
         <div className="card-content">
           <div className="card-img-container">
-            <a target="_blank" rel="noopener noreferrer" href="https://eyeseedocs.herokuapp.com/"><img src={ EyeDocs } alt="eye see docs" className="card-img"></img></a>
-            <h2 className="card-description">Eye See Docs</h2>
+            <a target="_blank" rel="noopener noreferrer" href="https://eyeseedocs.herokuapp.com/"><img src={ EyeDocs } alt="eye see docs" className="card-img"></img>
+            <div className="card-description">
+              <h1>Eye See Docs</h1>
+              <p>Documentation place for eye docs. Built using Python and Django.</p>
+            </div>
+            </a>
           </div>
         </div>
       </div>
@@ -34,8 +42,12 @@ const Project = () => {
       <div className="card">
         <div className="card-content">
           <div className="card-img-container">
-            <a target="_blank" rel="noopener noreferrer" href="https://the-number-slots.netlify.app/"><img src={ Slot } alt="slot machine" className="card-img"></img></a>
-            <h2 className="card-description">The Numbers Slot</h2>
+            <a target="_blank" rel="noopener noreferrer" href="https://the-number-slots.netlify.app/"><img src={ Slot } alt="slot machine" className="card-img"></img>
+            <div className="card-description">
+              <h1>The Numbers Slot</h1>
+              <p>Classic Vegas-style game built using Javascript, HTML, CSS.</p>
+            </div>
+            </a>
           </div>
         </div>
       </div>
@@ -43,8 +55,12 @@ const Project = () => {
       <div className="card">
         <div className="card-content">
           <div className="card-img-container">
-            <a target="_blank" rel="noopener noreferrer" href="https://make-your-marvel.herokuapp.com/"><img src={ Marvel } alt="make your marvel" className="card-img"></img></a>
-            <h2 className="card-description">Make Your Marvel</h2>
+            <a target="_blank" rel="noopener noreferrer" href="https://make-your-marvel.herokuapp.com/"><img src={ Marvel } alt="make your marvel" className="card-img"></img>
+            <div className="card-description">
+              <h1>Make Your Marvel</h1>
+              <p>Group endeavor. Built using MERN stack.</p>
+            </div>
+            </a>
           </div>
         </div>
       </div>
